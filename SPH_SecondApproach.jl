95a874f102239df617e9253ea6d49a7f581445c3
Ahmed Salih <>
2022-12-26 15:53:42 +0100

    Functioning single threaded code

--- SPH_SecondApproach.jl
+++ SPH_SecondApproach.jl
@@ -4,6 +4,7 @@ using StaticArrays
 using LinearAlgebra
 using CSV
 using DataFrames
+using Printf
 
 function Wᵢⱼ(αD,q)
     return αD*(1-q/2)^4*(2*q + 1)
@@ -30,17 +31,28 @@ function ∑ⱼWᵢⱼ(list,points,αD,h)
     return sumWI,sumWL
 end
 
-function ∇ᵢWᵢⱼ(αD,q,xᵢⱼ,h)
+# function ∇ᵢWᵢⱼ(αD,q,xᵢⱼ,h)
+#     # Skip distances outside the support of the kernel:
+#     if q < 0.0 || q > 2.0
+#         return SVector(0.0,0.0,0.0)
+#     end
+
+#     gradWx = αD * 1/h * (5*(q-2)^3*q)/8 * (xᵢⱼ[1] / (q*h+1e-6))
+#     gradWy = αD * 1/h * (5*(q-2)^3*q)/8 * (xᵢⱼ[2] / (q*h+1e-6))
+#     gradWz = αD * 1/h * (5*(q-2)^3*q)/8 * (xᵢⱼ[3] / (q*h+1e-6)) 
+
+#     return SVector(gradWx,gradWy,gradWz)
+# end
+
+# This is a much faster version of ∇ᵢWᵢⱼ
+function Optim∇ᵢWᵢⱼ(αD,q,xᵢⱼ,h) 
     # Skip distances outside the support of the kernel:
-    if q < 0.0 || q > 2.0
-        return SVector(0.0,0.0,0.0)
+    if 0 < q < 2
+        Fac = αD*5*(q-2)^3*q / (8h*(q*h+1e-6)) 
+    else
+        Fac = 0.0 # or return zero(xᵢⱼ) 
     end
-
-    gradWx = αD * 1/h * (5*(q-2)^3*q)/8 * (xᵢⱼ[1] / (q*h+1e-6))
-    gradWy = αD * 1/h * (5*(q-2)^3*q)/8 * (xᵢⱼ[2] / (q*h+1e-6))
-    gradWz = αD * 1/h * (5*(q-2)^3*q)/8 * (xᵢⱼ[3] / (q*h+1e-6)) 
-
-    return SVector(gradWx,gradWy,gradWz)
+    return Fac .* xᵢⱼ
 end
 
 function ∑ⱼ∇ᵢWᵢⱼ(list,points,αD,h)
@@ -55,7 +67,7 @@ function ∑ⱼ∇ᵢWᵢⱼ(list,points,αD,h)
 
         q = d/h
 
-        Wg = ∇ᵢWᵢⱼ(αD,q,xᵢⱼ,h)
+        Wg =  Optim∇ᵢWᵢⱼ(αD,q,xᵢⱼ,h)
 
         sumWgI[i] +=  Wg
         sumWgI[j] += -Wg
@@ -124,6 +136,18 @@ function ∂vᵢ∂t(system,points,m,ρ,WgL,c₀,γ,ρ₀)
     return dvdtI,dvdtL
 end
 
+
+function Δt(α,points,v,c₀,h,CFL)
+    eta2  = (0.01)h * (0.01)h
+    visc  = maximum(abs.(h* dot.(v,points) ./ (dot.(points,points) .+ eta2)))
+    dt1   = minimum(sqrt.(h ./ norm.(α)))
+    dt2   = h / (c₀+visc)
+
+    dt    = CFL*min(dt1,dt2)
+
+    return dt
+end
+
 function ListToIndex(points, list)
     out  = [ Int[] for _ in points ]
     rout = [ Float64[] for _ in points]
@@ -142,7 +166,7 @@ function ListToIndex(points, list)
         return out,rout
 end
 
-function create_vtp_file(filename,points,Wi,Wg,dρdt,dvdt,v)
+function create_vtp_file(filename,points,Wi,Wg,ρ,dvdt,v)
     # Convert the particle positions and densities into the format required by the vtk_grid function:
     points = reduce(hcat,points)  # Concatenate the particle positions into a single matrix
     polys = empty(MeshCell{WriteVTK.PolyData.Polys,UnitRange{Int64}}[])
@@ -158,26 +182,11 @@ function create_vtp_file(filename,points,Wi,Wg,dρdt,dvdt,v)
         # Add the particle densities as a point data array:
         vtk_point_data(vtk, Wi, "Wi")
         vtk_point_data(vtk, Wg, "Wg")
-        vtk_point_data(vtk, dρdt, "Density Derivative")
+        vtk_point_data(vtk, ρ, "Density")
         vtk_point_data(vtk, dvdt, "Acceleration")
         vtk_point_data(vtk, v, "Velocity")
     end
 end
-
-# ρ₀ = 1000
-# dx = 0.125/2
-# H  = sqrt(2)*dx
-# m₀ = ρ₀*dx'dx
-# mᵢ = mⱼ = m₀
-# αD = (7/(4*π*H^2))
-# c₀ = 100
-# γ  = 7
-
-# points   =  Iterators.product(-1.:dx:0., -1.:dx:0., 0:1:0.);
-# points   = vec(collect.(points))
-# points   = reinterpret.(SVector{3,Float64},points)
-# points   = map(x->x[1],points)
-
 ### Play with code
 DF_FLUID = CSV.read("FluidPoints_Dp0.02.csv", DataFrame)
 DF_BOUND = CSV.read("BoundaryPoints_Dp0.02.csv", DataFrame)
@@ -202,64 +211,71 @@ end
 GravityFactor = [Int64(-1) .+ 0*collect(1:size(DF_FLUID,1));Int64(1) .+ 0*collect(1:size(DF_BOUND,1))]
 MotionLimiter = [Int64(1)  .+ 0*collect(1:size(DF_FLUID,1));Int64(0) .+ 0*collect(1:size(DF_BOUND,1))]
 
-ρ₀ = 1000
-dx = 0.02
-H  = sqrt(2)*dx
-m₀ = ρ₀*dx'dx
-mᵢ = mⱼ = m₀
-αD = (7/(4*π*H^2))
-c₀ = 81#85.89
-γ  = 7
-g  = 9.81
-dt = 1e-5
 
-density  = Array([DF_FLUID.Rhop;DF_BOUND.Rhop])
-velocity = zeros(SVector{3,Float64},length(points))
-acceleration = zeros(SVector{3,Float64},length(points))
+function RunSimulation(points,GravityFactor,MotionLimiter)
+    foreach(rm, filter(endswith(".vtp"), readdir("./second_approach/",join=true)))
 
+    ρ₀  = 1000
+    dx  = 0.02
+    H   = sqrt(2)*dx
+    m₀  = ρ₀*dx*dx
+    mᵢ  = mⱼ = m₀
+    αD  = (7/(4*π*H^2))
+    c₀  = 81#85.89
+    γ   = 7
+    g   = 9.81
+    dt  = 1e-5
+    CFL = 0.3
 
+    density  = Array([DF_FLUID.Rhop;DF_BOUND.Rhop])
+    velocity = zeros(SVector{3,Float64},length(points))
+    acceleration = zeros(SVector{3,Float64},length(points))
 
-foreach(rm, filter(endswith(".vtp"), readdir("./second_approach/",join=true)))
-neighborlist!(system)
-for big_iter = 1:100001
     system  = InPlaceNeighborList(x=points, cutoff=2*H, parallel=false)
-    update!(system,points)
-    list = neighborlist!(system)
+    neighborlist!(system)
+    for big_iter = 1:100001
+        update!(system,points)
+        list = neighborlist!(system)
 
-    WiI,WiL = ∑ⱼWᵢⱼ(list,points,αD,H)
-    WgI,WgL = ∑ⱼ∇ᵢWᵢⱼ(list,points,αD,H)
+        WiI,_   = ∑ⱼWᵢⱼ(list,points,αD,H)
+        WgI,WgL = ∑ⱼ∇ᵢWᵢⱼ(list,points,αD,H)
 
-    dρdtI,dρdtL = ∂ρᵢ∂t(system,points,m₀,density,velocity,WgL)
+        dρdtI,_ = ∂ρᵢ∂t(system,points,m₀,density,velocity,WgL)
 
-    dvdtI,dvdtL = ∂vᵢ∂t(system,points,m₀,density,WgL,c₀,γ,ρ₀)
-    # We add gravity as a final step for the i particles, not the L ones, since we do not split the contribution, that is unphysical!
-    dvdtI .= map((x,y)->x+y*SVector(0,g,0),dvdtI,GravityFactor)
-
-
-    density_n_half  = density  .+ dρdtI * (dt/2)
-    velocity_n_half = velocity .+ dvdtI * (dt/2) .* MotionLimiter
-    points_n_half   = points   .+ velocity_n_half * (dt/2) .* MotionLimiter
-
-    dρdtI_n_half,dρdtL_n_half = ∂ρᵢ∂t(system,points_n_half,m₀,density_n_half,velocity_n_half,WgL)
-
-    dvdtI_n_half,dvdtL_n_half = ∂vᵢ∂t(system,points_n_half,m₀,density_n_half,WgL,c₀,γ,ρ₀)
-    dvdtI_n_half .= map((x,y)->x+y*SVector(0,g,0),dvdtI_n_half,GravityFactor) 
+        dvdtI,_ = ∂vᵢ∂t(system,points,m₀,density,WgL,c₀,γ,ρ₀)
+        # We add gravity as a final step for the i particles, not the L ones, since we do not split the contribution, that is unphysical!
+        dvdtI .= map((x,y)->x+y*SVector(0,g,0),dvdtI,GravityFactor)
 
 
-    epsi = -( dρdtI_n_half ./ density_n_half)*dt
+        density_n_half  = density  .+ dρdtI * (dt/2)
+        velocity_n_half = velocity .+ dvdtI * (dt/2) .* MotionLimiter
+        points_n_half   = points   .+ velocity_n_half * (dt/2) .* MotionLimiter
 
-    density_new   = density .* (2 .- epsi)./(2 .+ epsi)
-    velocity_new  = velocity .+ dvdtI_n_half * dt .* MotionLimiter
-    points_new    = points .+ ((velocity_new .+ velocity)/2) * dt .* MotionLimiter
+        dρdtI_n_half,_ = ∂ρᵢ∂t(system,points_n_half,m₀,density_n_half,velocity_n_half,WgL)
 
-    density  = density_new
-    velocity = velocity_new
-    points   = points_new
-    acceleration = dvdtI_n_half
+        dvdtI_n_half,_ = ∂vᵢ∂t(system,points_n_half,m₀,density_n_half,WgL,c₀,γ,ρ₀)
+        dvdtI_n_half .= map((x,y)->x+y*SVector(0,g,0),dvdtI_n_half,GravityFactor) 
 
-    if big_iter % 50 == 0
-        create_vtp_file("./second_approach/PlayAround_"*lpad(big_iter,4,"0"),points,WiI,WgI,density,acceleration,velocity)
+
+        epsi = -( dρdtI_n_half ./ density_n_half)*dt
+
+        density_new   = density .* (2 .- epsi)./(2 .+ epsi)
+        velocity_new  = velocity .+ dvdtI_n_half * dt .* MotionLimiter
+        points_new    = points .+ ((velocity_new .+ velocity)/2) * dt .* MotionLimiter
+
+        density      = density_new
+        velocity     = velocity_new
+        points       = points_new
+        acceleration = dvdtI_n_half
+
+        # Automatic time stepping probably does not work in non-vicous sim
+        # dt = Δt(acceleration,points,velocity,c₀,H,CFL)
+
+        @printf "Iteration %i | dt = %.5e \n" big_iter dt
+        if big_iter % 50 == 0
+            create_vtp_file("./second_approach/PlayAround_"*lpad(big_iter,4,"0"),points,WiI,WgI,density,acceleration,velocity)
+        end
     end
 end
 
-
+RunSimulation(points,GravityFactor,MotionLimiter)
