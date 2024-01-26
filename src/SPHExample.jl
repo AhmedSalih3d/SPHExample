67bff1a7d4a2426acb77b031f214564bb44f7511
AhmedSalih3d <36305327+AhmedSalih3d@users.noreply.github.com>
2024-01-26 17:40:45 +0100

    Check if PreProcess can be used this way to parse all functions to name space

--- src/SPHExample.jl
+++ src/SPHExample.jl
@@ -1,12 +1,13 @@
 module SPHExample
 
     include("AuxillaryFunctions.jl"); 
-    include("PreProcess.jl");         
+    include("PreProcess.jl");
+    using .PreProcess       
     include("PostProcess.jl");        
     include("TimeStepping.jl");       
     include("SimulationEquations.jl");
     
-    export LoadParticlesFromCSV
+    
 
 end
 
