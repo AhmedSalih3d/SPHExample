4d64049497f121461a5fe7f2fa78315557d91a53
AhmedSalih3d <36305327+AhmedSalih3d@users.noreply.github.com>
2024-03-07 18:49:00 +0100

    Aded kinematic viscosity

--- src/SimulationConstantsConfiguration.jl
+++ src/SimulationConstantsConfiguration.jl
@@ -50,6 +50,7 @@ constants = SimulationConstants(ρ₀=1017, dx=0.03, α=0.02)
     η²::T  = (0.01 * h)^2         ; @assert η²   > 0 "Eta squared (η²) must be positive"
     Cb::T  = (c₀^2 * ρ₀)/γ        ; @assert Cb  >= 0 "Cb (pressure coefficient) must be positive"
     Cb⁻¹::T  = inv(Cb)            ; @assert Cb⁻¹>= 0 "Inverse Cb (inverse pressure coefficient) must be positive"
+    ν₀::T    = 1e-6               ; @assert ν₀  >= 0 "Kinematic viscosity must be positive"
 end
 
 end
