2b11fc0ebb4a7419ef0ed70ce67b2b15b3e39bc9
AhmedSalih3d <36305327+AhmedSalih3d@users.noreply.github.com>
2024-03-13 13:22:15 +0100

    First version with extract cells working

--- example/TempRepoCLL_ActualRun_DDT_GPU.jl
+++ example/TempRepoCLL_ActualRun_DDT_GPU.jl
@@ -39,18 +39,18 @@ function ExtractCells!(cells, p,R,::Val{d}) where d
     return cells
 end
 
-function ExtractCells!(Cells, Points, Nmax=length(Cells))
+function ExtractCells!(Cells, Points, CutOff, Nmax=length(Cells))
     index  = (blockIdx().x - 1) * blockDim().x + threadIdx().x
     stride = gridDim().x * blockDim().x
-    @inbounds for i = index:stride:Nmax-1
-        Cells[i+1] =  Cells[i]
+    @inbounds for i = index:stride:Nmax
+        Cells[i] =  CartesianIndex(@. Int(fld(Points[i], CutOff)) ...)
     end
     return nothing
 end
 
 # Actual Function to Call for Kernel Gradient Calc
-function KernelExtractCells!(Cells, Points, Nmax=length(Cells))
-    kernel  = @cuda launch=false ExtractCells!(Cells, Points, Nmax)
+function KernelExtractCells!(Cells, Points, CutOff, Nmax=length(Cells))
+    kernel  = @cuda launch=false ExtractCells!(Cells, Points, CutOff, Nmax)
     config  = launch_configuration(kernel.fun)
     threads = min(Nmax, config.threads)
     blocks  = cld(Nmax, threads)
@@ -59,8 +59,6 @@ function KernelExtractCells!(Cells, Points, Nmax=length(Cells))
 end
 
 ###===
-
-
 
 Dimensions = 2
 FloatType  = Float64
@@ -71,7 +69,9 @@ BoundCSV   = "./input/still_wedge_mdbc/StillWedge_Dp0.02_Bound.csv"
 # @unpack HourGlass, SaveLocation, SimulationName, SilentOutput, ThreadsCPU = SimMetaData;
 
 # Unpack simulation constants
-# @unpack ρ₀, dx, h, m₀, αD, α, g, c₀, γ, dt, δᵩ, CFL, η² = SimConstants
+SimConstantsWedge = SimulationConstants{FloatType}(c₀=42.48576250492629)
+@unpack ρ₀, dx, h, m₀, αD, α, g, c₀, γ, dt, δᵩ, CFL, η² = SimConstantsWedge
+CutOff = 2h
 
 # Load in the fluid and boundary particles. Return these points and both data frames
 # @inline is a hack here to remove all allocations further down due to uncertainty of the points type at compile time
@@ -88,8 +88,8 @@ cuVelocity     = similar(cuPosition)
 cuCells        = similar(cuPosition, CartesianIndex{Dimensions})
 
 ###= Preallocate functions and sizes for GPU exec
-FuncExtractCells!, ThreadsExtractCells!, BlocksExtractCells! = KernelExtractCells!(cuCells,cuPosition)
-FunctionExtractCells!(cuCells,cuPosition) = @cuda threads=ThreadsExtractCells! blocks=BlocksExtractCells!  ExtractCells!(cuCells,cuPosition)
+FuncExtractCells!, ThreadsExtractCells!, BlocksExtractCells! = KernelExtractCells!(cuCells,cuPosition,CutOff)
+FunctionExtractCells!(cuCells,cuPosition) = @cuda threads=ThreadsExtractCells! blocks=BlocksExtractCells!  ExtractCells!(cuCells,cuPosition,CutOff)
 ###=
 
 FunctionExtractCells!(cuCells,cuPosition)
