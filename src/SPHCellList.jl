40bef4a5f9f1913b8fa14e34d60d00d257f66158
AhmedSalih3d <36305327+AhmedSalih3d@users.noreply.github.com>
2024-03-25 14:09:27 +0100

    inline small funcs

--- src/SPHCellList.jl
+++ src/SPHCellList.jl
@@ -17,7 +17,7 @@ include("AuxillaryFunctions.jl"); using .AuxillaryFunctions
         return n
     end
 
-    function ExtractCells!(Cells, Points, CutOff)
+    @inline function ExtractCells!(Cells, Points, CutOff)
         for i ∈ eachindex(Cells)
             Cells[i]  =  CartesianIndex(@. Int(fld(Points[i], CutOff)) ...)
             Cells[i] +=  2 * one(Cells[i])  # + CartesianIndex(1,1) + CartesianIndex(1,1) #+ ZeroOffset + HalfPad
