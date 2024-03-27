29f3b32e2780a1e53ae74ac75444d494676e2a2a
AhmedSalih3d <36305327+AhmedSalih3d@users.noreply.github.com>
2024-03-28 00:42:43 +0100

    Removed rearrange vectors

--- src/AuxillaryFunctions.jl
+++ src/AuxillaryFunctions.jl
@@ -1,22 +1,10 @@
 module AuxillaryFunctions
-using Bumper, StaticArrays
+using StaticArrays
 
-export RearrangeVector!, ResetArrays!, to_3d
+export ResetArrays!, to_3d
 
 ResetArrays!(arrays...) = foreach(a -> fill!(a, zero(eltype(a))), arrays)
 
 to_3d(vec_2d) = [SVector(v..., 0.0) for v in vec_2d]
-
-# Proper in-place re-arranging of vector
-function RearrangeVector!(vec1,indices)
-    buf = default_buffer()
-    @no_escape buf begin
-        temp  = @alloc(eltype(vec1),length(vec1))
-
-        temp .= @view vec1[indices]
-        vec1 .= temp
-    end
-end
-
 
 end
