1e2b254c5ea596a12fc893f234eadff69529b3d3
AhmedSalih3d <36305327+AhmedSalih3d@users.noreply.github.com>
2024-03-30 00:17:38 +0100

    Subhan Allah, reducing cutting radius for gridding algorithm, lowers neighborloop time but increases add index counter. Seems to be worthwhile though.

--- example/StillWedge.jl
+++ example/StillWedge.jl
@@ -271,7 +271,7 @@ let
     SimMetaDataWedge  = SimulationMetaData{Dimensions,FloatType}(
         SimulationName="Test", 
         SaveLocation="E:/SecondApproach/TESTING_CPU",
-        SimulationTime=1,
+        SimulationTime=4,
         OutputEach=0.01,
         FlagDensityDiffusion=true,
         FlagOutputKernelValues=true,
