21685f266fc8c5bae6f0b24d0ad9cb17c843543a
AhmedSalih3d <36305327+AhmedSalih3d@users.noreply.github.com>
2024-03-30 17:53:25 +0100

    + 2 double speed instead of one(cartesianindex())

--- test/benchmark_extract_cells.jl
+++ test/benchmark_extract_cells.jl
@@ -6,32 +6,10 @@ using Base.Threads
 
 function BatchExtractCells!(Cells, Points, CutOff)
     @batch per=thread for i ∈ eachindex(Cells)
-        ci = CartesianIndex(@. Int(fld(Points[i], CutOff))...)
-        Cells[i] = ci + 2 * one(ci) 
+        Cells[i] = CartesianIndex(@. Int(fld(Points[i] + 2, CutOff))...)
     end
 end
 
-function BatchExtractCellsManual!(Cells, Points, CutOff)
-    @batch per=thread for i in eachindex(Cells)
-        p = Points[i]
-        x, y = p[1], p[2]
-        
-        ci_x = Int(fld(x, CutOff))
-        ci_y = Int(fld(y, CutOff))
-
-        ci = CartesianIndex(ci_x, ci_y)
-        Cells[i] = ci + 2 * one(ci)
-    end
-end
-
-
-
-# function ThreadsExtractCells!(Cells, Points, CutOff)
-#     @threads for i ∈ eachindex(Cells)
-#         ci = CartesianIndex(@. Int(fld(Points[i], CutOff))...)
-#         Cells[i] = ci + 2 * one(ci) 
-#     end
-# end
 
 # Number of points/cells to test
 N         = 100_000
@@ -49,10 +27,5 @@ Cells = [zero(CartesianIndex{Dims}) for _ = 1:N]
 benchmark_result = @b BatchExtractCells!($Cells, $Points, $CutOff)
 println("BatchExtractCells"); display(benchmark_result)
 
-@benchmark BatchExtractCells!($Cells, $Points, $CutOff)
+# @benchmark BatchExtractCells!($Cells, $Points, $CutOff)
 
-benchmark_result = @b BatchExtractCellsManual!($Cells, $Points, $CutOff)
-println("BatchExtractCellsManual"); display(benchmark_result)
-
-# benchmark_result = @b ThreadsExtractCells!($Cells, $Points, $CutOff)
-# println("ThreadsExtractCells"); display(benchmark_result)
