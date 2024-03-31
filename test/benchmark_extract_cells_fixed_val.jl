18074dd599f2d2d1b747cadeaeaf0d383a4eecae
AhmedSalih3d <36305327+AhmedSalih3d@users.noreply.github.com>
2024-03-31 17:44:05 +0200

    trunc for the win!

--- test/benchmark_extract_cells_fixed_val.jl
+++ test/benchmark_extract_cells_fixed_val.jl
@@ -12,33 +12,14 @@ function BatchExtractCellsOriginal!(Cells, Points, CutOff)
     end
 end
 
-function BatchExtractCellsFloorOriginal!(Cells, Points, CutOff)
-    @batch per=thread for i ∈ eachindex(Cells)
-        Cells[i] = CartesianIndex(@. floor(Int, 2 + Points[i] / CutOff)...)
-    end
-end
-
-function BatchExtractCellsFloorVal!(Cells, Points, ::Val{CutOff}) where CutOff
-    @batch per=thread for i ∈ eachindex(Cells)
-        Cells[i] = CartesianIndex(@. floor(Int, 2 + Points[i] / CutOff)...)
-    end
-end
-
-function BatchExtractCellsFloorMap!(Cells, Points, ::Val{CutOff}) where CutOff
+function BatchExtractCellsMuladd!(Cells, Points, ::Val{InverseCutOff}) where InverseCutOff
     function map_floor(x)
-        @fastmath floor(Int, x / CutOff) + 2
+        unsafe_trunc(Int, muladd(x,InverseCutOff,2))
     end
-    
+
     @batch per=thread for i ∈ eachindex(Cells)
         t = map(map_floor, Tuple(Points[i]))
         Cells[i] = CartesianIndex(t)
-    end
-end
-
-# CutOff fixed to 0.5
-function BatchExtractCellsFloorFixed!(Cells, Points, _)
-    @batch per=thread for i ∈ eachindex(Cells)
-        Cells[i]   = CartesianIndex(@. floor(Int, 2 + Points[i] / 0.5)...)
     end
 end
 
@@ -48,6 +29,8 @@ let
     Dims      = 2 
     FloatType = Float64
     CutOff    = 0.5
+    InverseCutOff = 1/CutOff
+    ValInverseCutOff = Val(InverseCutOff)
 
     # Initialize Points with random SVector values
     Points = [SVector{Dims, FloatType}(rand(Dims)...) for _ = 1:N]
@@ -56,32 +39,18 @@ let
     Cells = [zero(CartesianIndex{Dims}) for _ = 1:N]
 
     # Benchmarks
-    benchmark_result = @b BatchExtractCellsOriginal!($Cells, $Points, $CutOff)
+    benchmark_result = @benchmark BatchExtractCellsOriginal!($Cells, $Points, $CutOff)
     println("BatchExtractCellsOriginal!"); display(benchmark_result)
+    
+    benchmark_result = @benchmark BatchExtractCellsMuladd!($Cells, $Points, $ValInverseCutOff)
+    println("BatchExtractCellsMuladd!"); display(benchmark_result)
 
-    benchmark_result = @b BatchExtractCellsFloorOriginal!($Cells, $Points, $CutOff)
-    println("BatchExtractCellsFloorOriginal!"); display(benchmark_result)
-
-    benchmark_result = @b BatchExtractCellsFloorVal!($Cells, $Points, $(Val(CutOff)))
-    println("BatchExtractCellsFloorVal!"); display(benchmark_result)
-
-    benchmark_result = @b BatchExtractCellsFloorMap!($Cells, $Points, $(Val(CutOff)))
-    println("BatchExtractCellsFloorMap!"); display(benchmark_result)
-
-    benchmark_result = @b BatchExtractCellsFloorFixed!($Cells, $Points, $CutOff)
-    println("BatchExtractCellsFloorFixed!"); display(benchmark_result)
 
     Cells0 = [zero(CartesianIndex{Dims}) for _ = 1:N]
     CellsA = [zero(CartesianIndex{Dims}) for _ = 1:N]
-    CellsB = [zero(CartesianIndex{Dims}) for _ = 1:N]
-    CellsC = [zero(CartesianIndex{Dims}) for _ = 1:N]
-    CellsD = [zero(CartesianIndex{Dims}) for _ = 1:N]
 
     BatchExtractCellsOriginal!(Cells0, Points, CutOff)
-    BatchExtractCellsFloorOriginal!(CellsA, Points, CutOff)
-    BatchExtractCellsFloorVal!(CellsB, Points, Val(CutOff))
-    BatchExtractCellsFloorMap!(CellsC, Points, Val(CutOff))
-    BatchExtractCellsFloorFixed!(CellsD, Points, CutOff)
+    BatchExtractCellsMuladd!(CellsA, Points, ValInverseCutOff)
 
-    @test Cells0 == CellsA == CellsB == CellsC == CellsD
+    @test Cells0 == CellsA
 end
