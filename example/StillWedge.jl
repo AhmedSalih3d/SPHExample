80736867737c44cf447af9880976c5440766a4f2
AhmedSalih3d <36305327+AhmedSalih3d@users.noreply.github.com>
2024-04-12 11:27:42 +0200

    confirm

--- example/StillWedge.jl
+++ example/StillWedge.jl
@@ -328,7 +328,7 @@ let
     SimMetaDataWedge  = SimulationMetaData{Dimensions,FloatType}(
         SimulationName="StillWedge", 
         SaveLocation="E:/SecondApproach/TESTING_CPU_StillWedge",
-        SimulationTime=0.01,
+        SimulationTime=1,
         OutputEach=0.01,
         FlagDensityDiffusion=true,
         FlagOutputKernelValues=false,
