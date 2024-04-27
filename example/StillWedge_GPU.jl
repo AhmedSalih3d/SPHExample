9363a516c4bb53596d7599e645bbcbafbcc87054
AhmedSalih3d <36305327+AhmedSalih3d@users.noreply.github.com>
2024-04-27 21:15:03 +0200

    One step at a time

--- example/StillWedge_GPU.jl
+++ example/StillWedge_GPU.jl
@@ -6,25 +6,25 @@ using CUDA
 Dimensions = 2
 FloatType  = Float64
 
-SimConstantsWedge = SimulationConstants{FloatType}(dx=0.02,c₀=42.48576250492629, δᵩ = 0.1, CFL=0.2)
+SimConstants = SimulationConstants{FloatType}(dx=0.02,c₀=42.48576250492629, δᵩ = 0.1, CFL=0.2)
 
 # Define the dictionary with specific types for keys and values to avoid any type ambiguity
 SimGeometry = Dict{Symbol, Dict{String, Union{String, Int, ParticleType, Nothing}}}()
 
 # Populate the dictionary
 SimGeometry[:FixedBoundary] = Dict(
-    "CSVFile"     => "./input/still_wedge/StillWedge_Dp$(SimConstantsWedge.dx)_Bound.csv",
+    "CSVFile"     => "./input/still_wedge/StillWedge_Dp$(SimConstants.dx)_Bound.csv",
     "GroupMarker" => 1,
     "Type"        => Fixed,
     "Motion"      => nothing
 )
 SimGeometry[:Water] = Dict(
-    "CSVFile"     => "./input/still_wedge/StillWedge_Dp$(SimConstantsWedge.dx)_Fluid.csv",
+    "CSVFile"     => "./input/still_wedge/StillWedge_Dp$(SimConstants.dx)_Fluid.csv",
     "GroupMarker" => 2,
     "Type"        => Fluid,
     "Motion"      => nothing
 )
-SimMetaDataWedge  = SimulationMetaData{Dimensions,FloatType}(
+SimMetaData  = SimulationMetaData{Dimensions,FloatType}(
     SimulationName="StillWedge", 
     SaveLocation="E:/SecondApproach/TESTING_CPU_StillWedge",
     SimulationTime=4,
@@ -52,3 +52,52 @@ Positionₙ⁺_GPU   = replace_storage(CuVector, Positionₙ⁺)
 
 # Disable scalar operations for performance optimization in CUDA operations
 CUDA.allowscalar(false)
+
+NumberOfPoints::Int = length(SimParticles)
+
+# Produce sorting related variables
+ParticleRanges         = CUDA.zeros(Int, NumberOfPoints + 1)
+UniqueCells            = CUDA.zeros(CartesianIndex{Dimensions}, NumberOfPoints)
+Stencil                = ConstructStencil(Val(Dimensions))
+
+InverseCutOff = Val(1/(SimConstants.H))
+
+
+# @inline function ExtractCells!(Particles, ::Val{InverseCutOff}) where InverseCutOff
+#     # Replace unsafe_trunc with trunc if this ever errors
+#     function map_floor(x)
+#         unsafe_trunc(Int, muladd(x,InverseCutOff,2))
+#     end
+
+#     Cells  = @views Particles.Cells
+#     Points = @views Particles.Position
+#     @threads for i ∈ eachindex(Particles)
+#         t = map(map_floor, Tuple(Points[i]))
+#         Cells[i] = CartesianIndex(t)
+#     end
+#     return nothing
+# end
+
+# ###=== Function to update ordering
+# function UpdateNeighbors!(Particles, CutOff, SortingScratchSpace, ParticleRanges, UniqueCells)
+#     ExtractCells!(Particles, CutOff)
+
+#     sort!(Particles, by = p -> p.Cells; scratch=SortingScratchSpace)
+
+#     Cells = @views Particles.Cells
+#     @. ParticleRanges             = zero(eltype(ParticleRanges))
+#     IndexCounter                  = 1
+#     ParticleRanges[IndexCounter]  = 1
+#     UniqueCells[IndexCounter]     = Cells[1]
+
+#     for i in 2:length(Cells)
+#         if Cells[i] != Cells[i-1] # Equivalent to diff(Cells) != 0
+#             IndexCounter                += 1
+#             ParticleRanges[IndexCounter] = i
+#             UniqueCells[IndexCounter]    = Cells[i]
+#         end
+#     end
+#     ParticleRanges[IndexCounter + 1]  = length(ParticleRanges)
+
+#     return IndexCounter 
+# end
