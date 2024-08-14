e9dfe4815246850c8b8d748898bf1e9abeaf5640
AhmedSalih3d <36305327+AhmedSalih3d@users.noreply.github.com>
2024-08-14 14:56:19 +0200

    Added a function to close hdf files manually, if one breaks simulation before hand by crtl+c. Only needs directory

--- src/AuxillaryFunctions.jl
+++ src/AuxillaryFunctions.jl
@@ -1,10 +1,30 @@
 module AuxillaryFunctions
 using StaticArrays
+using Base.Threads
+using HDF5
 
-export ResetArrays!, to_3d
+export ResetArrays!, to_3d, CloseHDFVTKManually
 
 ResetArrays!(arrays...) = foreach(a -> fill!(a, zero(eltype(a))), arrays)
 
 to_3d(vec_2d) = [SVector(v..., 0.0) for v in vec_2d]
+
+function CloseHDFVTKManually(directory_path::String)
+    # Get all files in the directory
+    all_files = readdir(directory_path, join=true)
+
+    # Filter out the files that end with .vtkhdf
+    vtkhdf_files = filter(file -> endswith(file, ".vtkhdf"), all_files)
+    
+    # Process each file in a threaded manner
+    @threads for file_path in vtkhdf_files
+        file = h5open(file_path, "r")
+        try
+            close(file)
+        catch e
+            @warn(e)
+        end
+    end
+end
 
 end
