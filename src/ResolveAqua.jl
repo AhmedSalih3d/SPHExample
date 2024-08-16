389df574cbd4dc38fc01bcecdb43ea17dc382fc9
AhmedSalih3d <36305327+AhmedSalih3d@users.noreply.github.com>
2024-08-16 14:51:27 +0200

    renable precompile

--- src/ResolveAqua.jl
+++ src/ResolveAqua.jl
@@ -1,4 +1,4 @@
-__precompile__(false)
+#https://discourse.julialang.org/t/how-to-fix-ambiguities-found-through-aqua-jl/118269/2
 
 using SentinelArrays
 using StructArrays: StructArray
@@ -22,17 +22,11 @@ Base.:(==)(a::SentinelArrays.ChainedVectorIndex, b::BigInt) = ==(BigInt(a), b)
 # Resolving ambiguity #6
 Base.:(==)(a::BigInt, b::SentinelArrays.ChainedVectorIndex) = ==(a, BigInt(b))
 
-# Resolving ambiguity #7
-Base.Broadcast.broadcasted(f::F, A::SentinelArrays.ChainedVector) where F = Base.Broadcast.broadcasted(f, collect(A))
-
 # Resolving ambiguity #8
 Base.copyto!(dest::PermutedDimsArray{T, 1}, src::SentinelArrays.ChainedVector) where T = Base.copyto!(dest, collect(src))
 
 # Resolving ambiguity #9
 Base.copyto!(dest::PermutedDimsArray{T, 1}, src::SentinelArrays.ChainedVector{T, A} where A<:AbstractVector{T}) where T = Base.copyto!(dest, collect(src))
-
-# Resolving ambiguity #11
-Base.findall(f::Function, x::SentinelArrays.ChainedVector) = Base.findall(f, collect(x))
 
 # Resolving ambiguity #12
 Base.reduce(::typeof(vcat), x::SentinelArrays.ChainedVector{T, A} where {T<:(AbstractVecOrMat), A<:AbstractVector{T}}) = Base.reduce(vcat, collect(x))
@@ -44,9 +38,6 @@ Base.reduce(::typeof(hcat), x::SentinelArrays.ChainedVector{T, A} where {T<:(Abs
 Base.reshape(s::StructArray{T}, d::Tuple{Integer, Vararg{Integer}}) where T = Base.reshape(collect(s), d)
 
 # Resolving ambiguity #15
-Base.similar(s::StructArray, S::Type, sz::Tuple{Integer, Vararg{Integer}}) = Base.similar(collect(s), S, sz)
-
-# Resolving ambiguity #16
 Base.similar(s::StructArray, S::Type, sz::Tuple{Integer, Vararg{Integer}}) = Base.similar(collect(s), S, sz)
 
 # Resolving Ambiguity #1
