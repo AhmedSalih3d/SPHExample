5f944722fe71eb85044af9a2c99a70fa6f44e266
AhmedSalih3d <36305327+AhmedSalih3d@users.noreply.github.com>
2024-08-23 21:47:16 +0200

    Setting up mdbc example file

---
+++ example/StillWedge_mDBC.jl
@@ -0,0 +1,53 @@
+using SPHExample
+
+let
+    Dimensions = 2
+    FloatType  = Float64
+
+    
+    SimConstantsWedge = SimulationConstants{FloatType}(dx=0.02,c₀=42.48576250492629, δᵩ = 0.1, CFL=0.2)
+
+    # Define the dictionary with specific types for keys and values to avoid any type ambiguity
+    SimulationGeometry = Dict{Symbol, Dict{String, Union{String, Int, ParticleType, Nothing}}}()
+
+    # Populate the dictionary
+    SimulationGeometry[:FixedBoundary] = Dict(
+        "CSVFile"     => "./input/still_wedge_mdbc/StillWedge_Dp$(SimConstantsWedge.dx)_Bound.csv",
+        "GroupMarker" => 1,
+        "Type"        => Fixed,
+        "Motion"      => nothing
+    )
+
+    SimulationGeometry[:Water] = Dict(
+        "CSVFile"     => "./input/still_wedge_mdbc/StillWedge_Dp$(SimConstantsWedge.dx)_Fluid.csv",
+        "GroupMarker" => 2,
+        "Type"        => Fluid,
+        "Motion"      => nothing
+    )
+
+
+    SimMetaDataWedge  = SimulationMetaData{Dimensions,FloatType}(
+        SimulationName="StillWedge", 
+        SaveLocation="E:/SecondApproach/TESTING_CPU_StillWedge",
+        SimulationTime=4,
+        OutputEach=0.01,
+        VisualizeInParaview=true,
+        OpenLogFile=true,
+        FlagDensityDiffusion=true,
+        FlagOutputKernelValues=false,
+        FlagLog=true,
+        FlagShifting=false,
+    )
+
+    SimLogger = SimulationLogger(SimMetaDataWedge.SaveLocation)
+
+    CleanUpSimulationFolder(SimMetaDataWedge.SaveLocation)
+
+    @profview RunSimulation(
+        SimGeometry        = SimulationGeometry,
+        SimMetaData        = SimMetaDataWedge,
+        SimConstants       = SimConstantsWedge,
+        SimLogger          = SimLogger
+    )
+end
+
