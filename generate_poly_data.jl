1277fb6648597e66eb73bcb7b5c005c3684bddac
AhmedSalih3d <36305327+AhmedSalih3d@users.noreply.github.com>
2025-02-25 23:57:44 +0100

    Closing in in shaa Allah on working solution

---
+++ generate_poly_data.jl
@@ -0,0 +1,108 @@
+using HDF5
+
+fType  = Float64
+idType = Int8
+
+connectivities = ["Vertices", "Lines", "Polygons", "Strips"]
+
+function generate_geometry_structure(root)
+    # Write version of VTKHDF format as an attribute
+    HDF5.attrs(root)["Version"] = [2, 0]
+    
+    # Write type of dataset ("PolyData") as an ASCII string to a "Type" attribute.
+    # This is a bit tricky because VTK/ParaView don't support UTF-8 strings here, which is
+    # the default in HDF5.jl.
+    let s = "PolyData"
+        dtype = HDF5.datatype(s)
+        HDF5.API.h5t_set_cset(dtype.id, HDF5.API.H5T_CSET_ASCII)
+        dspace = HDF5.dataspace(s)
+        attr = HDF5.create_attribute(root, "Type", dtype, dspace)
+        HDF5.write_attribute(attr, dtype, s)
+    end
+    
+    NumberOfPoints = HDF5.create_dataset(root, "NumberOfPoints" , idType , ((0,),(-1,)), chunk=(100,))
+    Points         = HDF5.create_dataset(root, "Points"         , fType  , ((0,3), (-1,3)), chunk=(100,3))
+
+    for connect in connectivities
+        group = HDF5.create_group(root, connect)
+
+        NumberOfConnectivityIds = HDF5.create_dataset(group, "NumberOfConnectivityIds", idType, ((0,),(-1,)), chunk=(100,) )
+        NumberOfCells           = HDF5.create_dataset(group, "NumberOfCells          ", idType, ((0,),(-1,)), chunk=(100,) )
+        Offsets                 = HDF5.create_dataset(group, "Offsets                ", idType, ((0,),(-1,)), chunk=(100,) )
+        Connectivity            = HDF5.create_dataset(group, "Connectivity           ", idType, ((0,),(-1,)), chunk=(100,) )
+    end
+
+    pData = HDF5.create_group(root, "PointData")
+    Warping = HDF5.create_dataset(pData, "Warping", fType, ((0,3),(-1,3)), chunk=(100,3))
+    Normals = HDF5.create_dataset(pData, "Normals", fType, ((0,3),(-1,3)), chunk=(100,3))
+
+    cData     = HDF5.create_group(root, "CellData")
+    Materials = HDF5.create_dataset(cData, "Materials", idType, ((0,),(-1,)), chunk=(100,))
+
+    return nothing
+end
+
+function generate_step_structure(root)
+    steps = HDF5.create_group(root, "Steps")
+    # HDF5.attrs(steps)["NSteps"] = 0
+
+    NSteps, _ = HDF5.create_attribute(steps, "NSteps", Int)
+
+    Values = HDF5.create_dataset(steps, "Values", fType , ((0,),(-1,)), chunk=(100,))
+
+    singleDSs = ["PartOffsets", "NumberOfParts", "PointOffsets"]
+    for name in singleDSs
+        HDF5.create_dataset(steps, name, idType, ((0,),(-1,)), chunk=(100,))
+    end
+
+    nTopoDSs = ["CellOffsets", "ConnectivityIdOffsets"]
+    for name in nTopoDSs
+        HDF5.create_dataset(steps, name, idType, ((0,4),(-1,4)), chunk=(100,4))
+    end
+    
+    pData = HDF5.create_group(steps, "PointDataOffsets")
+    Warping = HDF5.create_dataset(pData, "Warping", idType, ((0,),(-1,)), chunk=(100,))
+    Normals = HDF5.create_dataset(pData, "Normals", idType, ((0,),(-1,)), chunk=(100,))
+
+    cData     = HDF5.create_group(steps, "CellDataOffsets")
+    Materials = HDF5.create_dataset(cData, "Materials", idType, ((0,),(-1,)), chunk=(100,))
+end
+
+function append_data(root, poly_data, newStep=nothing, geometryOffset=nothing)
+
+    if true #!isnothing(newStep)
+        steps = root["Steps"]
+        print(HDF5.read_attribute(steps, "NSteps"))
+        steps["NSteps"] = 2
+    end
+
+end
+
+function generate_data(root)
+    generate_geometry_structure(root)
+    generate_step_structure(root)
+
+    ts = range(0,0.5,6)
+
+    for (iT, t) in enumerate(ts)
+
+    end
+end
+
+
+f = h5open("test.vtkhdf", "w")
+root = HDF5.create_group(f, "VTKHDF")
+generate_data(root)
+display(f)
+
+attributes(steps)
+steps = root["Steps"]
+steps_attr_dict = attributes(steps)
+NSteps = steps_attr_dict["NSteps"]
+println(HDF5.read_attribute(steps, "NSteps"))
+write_attribute(NSteps,HDF5.datatype(idType), 4)
+
+print(HDF5.read_attribute(steps, "NSteps"))
+
+
+close(f)
