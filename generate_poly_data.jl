bb45b135ab478cba9d04d196cf7d072a606bdfbe
AhmedSalih3d <36305327+AhmedSalih3d@users.noreply.github.com>
2025-03-08 12:17:53 +0100

    Alhamdu lillah

--- generate_poly_data.jl
+++ generate_poly_data.jl
@@ -1,13 +1,24 @@
 using HDF5
+using StaticArrays
 
 fType  = Float64
 idType = Int8
 
 connectivities = ["Vertices", "Lines", "Polygons", "Strips"]
 
+
+Positions = SVector{3, Float64}[
+    [0.0, 0.0, 0.5],
+    [0.0, 0.0, -0.5],
+    [0.5, 0.0, 3.061617e-17],
+    [-0.25, 0.4330127, 3.061617e-17],
+    [-0.25, -0.4330127, 3.061617e-17]
+]
+
+
 function generate_geometry_structure(root)
     # Write version of VTKHDF format as an attribute
-    HDF5.attrs(root)["Version"] = [2, 0]
+    HDF5.attrs(root)["Version"] = Int32.([2, 0])
     
     # Write type of dataset ("PolyData") as an ASCII string to a "Type" attribute.
     # This is a bit tricky because VTK/ParaView don't support UTF-8 strings here, which is
@@ -21,7 +32,7 @@ function generate_geometry_structure(root)
     end
     
     NumberOfPoints = HDF5.create_dataset(root, "NumberOfPoints" , idType , ((0,),(-1,)), chunk=(100,))
-    Points         = HDF5.create_dataset(root, "Points"         , fType  , ((0,3), (-1,3)), chunk=(100,3))
+    Points         = HDF5.create_dataset(root, "Points"         , fType  , ((3,0), (3,-1)), chunk=(3,100))
 
     for connect in connectivities
         group = HDF5.create_group(root, connect)
@@ -46,7 +57,7 @@ function generate_step_structure(root)
     steps = HDF5.create_group(root, "Steps")
     # HDF5.attrs(steps)["NSteps"] = 0
 
-    NSteps, _ = HDF5.create_attribute(steps, "NSteps", Int)
+    NSteps, _ = HDF5.create_attribute(steps, "NSteps", Int32)
 
     Values = HDF5.create_dataset(steps, "Values", fType , ((0,),(-1,)), chunk=(100,))
 
@@ -68,50 +79,71 @@ function generate_step_structure(root)
     Materials = HDF5.create_dataset(cData, "Materials", idType, ((0,),(-1,)), chunk=(100,))
 end
 
-function append_data(root, poly_data, newStep=nothing, geometryOffset=nothing)
 
-    if !isnothing(newStep)
+
+function append_data(root, newStep, Positions)
+
         steps = root["Steps"]
+        attrs(steps)["NSteps"] = Int32(attrs(steps)["NSteps"] + 1)
 
-        old_NSteps = HDF5.read_attribute(steps, "NSteps")
+        HDF5.set_extent_dims(steps["Values"], (length(steps["Values"]) + 1,))
+        steps["Values"][end] = newStep
 
-        steps_attr_dict = attributes(steps)
-        NSteps = steps_attr_dict["NSteps"]
-        write_attribute(NSteps,HDF5.datatype(idType), old_NSteps + 1)
+        PointsStartIndex = length(root["Points"]) + 1
+        PositionLength   = length(Positions)
+        HDF5.set_extent_dims(root["Points"], (length(first(Positions)), PositionLength))
+        root["Points"][:, PointsStartIndex:(PointsStartIndex+PositionLength-1)] = stack(Positions)
 
-        geomOffs = []
-        if isnothing(geometryOffset)
-            append!(geomOffs, size(root["NumberOfPoints"])[1])
-            append!(geomOffs, 1)
-            append!(geomOffs, size(root["Points"][1]))
+        # old_NSteps = HDF5.read_attribute(steps, "NSteps")
 
-            for connect in connectivities
-                append!(geomOffs, size(root[connect]["Offsets"])[1] - geomOffs[1])
-            end
+        # steps_attr_dict = attributes(steps)
+        # NSteps = steps_attr_dict["NSteps"]
+        # write_attribute(NSteps,HDF5.datatype(idType), old_NSteps + 1)
 
-            for connect in connectivities
-                append!(geomOffs, size(root[connect]["Connectivity"][1]))
-            end
-        end
-    end
+        # geomOffs = []
+        # if isnothing(geometryOffset)
+        #     append!(geomOffs, size(root["NumberOfPoints"])[1])
+        #     append!(geomOffs, 1)
+        #     append!(geomOffs, size(root["Points"][1]))
+
+        #     for connect in connectivities
+        #         append!(geomOffs, size(root[connect]["Offsets"])[1] - geomOffs[1])
+        #     end
+
+        #     for connect in connectivities
+        #         append!(geomOffs, size(root[connect]["Connectivity"][1]))
+        #     end
+        # else
+        #     append!(geomOffs, steps["PartOffsets"][geometryOffset])
+        #     append!(geomOffs, 1)
+        #     append!(geomOffs, steps["PointOffsets"][geometryOffset])
+
+        #     for (iC,_) in enumerate(connectivities)
+        #         append!(geomOffs, steps["CellOffsets"][geometryOffset, iC])
+        #     end
+
+        #     for connect in connectivities
+        #         append!(geomOffs, steps["ConnectivityIdOffsets"][geometryOffset, iC])
+        #     end
+        # end
 
 end
 
-function generate_data(root)
+function generate_data(root, positions)
     generate_geometry_structure(root)
     generate_step_structure(root)
 
-    ts = range(0,0.5,6)
+    ts = 0 #range(0,0.5,1)
 
     for (iT, t) in enumerate(ts)
-
+        append_data(root, t, positions)
     end
 end
 
 
 f = h5open("test.vtkhdf", "w")
 root = HDF5.create_group(f, "VTKHDF")
-generate_data(root)
+generate_data(root, Positions)
 display(f)
 
 close(f)
