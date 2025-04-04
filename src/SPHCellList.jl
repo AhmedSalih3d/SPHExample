7a9b531258ecf627b2656e94e6ad63f45a4b6992
AhmedSalih3d <36305327+AhmedSalih3d@users.noreply.github.com>
2025-04-05 01:30:29 +0200

    Close!

--- src/SPHCellList.jl
+++ src/SPHCellList.jl
@@ -530,18 +530,20 @@ using UnicodePlots
                     if condinf <= 50
                         GhostPointDensity = bᵧ[i]' * InvA
                         val = GhostPointDensity[1] + dot(Positionₙ⁺[i] - GhostPoints[i], GhostPointDensity[2:end])  #InvA[1,1] * bᵧ[i][1] + InvA[1,2] * bᵧ[i][2] + InvA[1,3] * bᵧ[i][3] # + dot(GhostNormals[i], GhostPointDensity[2:end])
+                        # println("GhostKernel True: ", val)
                     else
                         val = first(bᵧ[i]) / first(Aᵧ[i])
+                        # println("GhostKernel False: ", val)
                     end
 
                     # println("| val:", val, "| ρₙ⁺[i]:", ρₙ⁺[i], "| GhostKernel[i]:", GhostKernel[i])
-                    println("GhosKernel: ", val)
+                   
                 end
                 
             else
-                val = max(SimConstants.ρ₀, ρⱼ / first(Aᵧ[i]))
+                val = SimConstants.ρ₀ #max(SimConstants.ρ₀, ρⱼ / first(Aᵧ[i]))
 
-                println("Else: ", val)
+                # println("Else: ", val)
             end
 
            
