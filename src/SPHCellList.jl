d5a5cf5500f7394173cffc0708d505b06d89e226
AhmedSalih3d <36305327+AhmedSalih3d@users.noreply.github.com>
2025-04-06 12:22:55 +0200

    Testing first mdbc, simpler

--- src/SPHCellList.jl
+++ src/SPHCellList.jl
@@ -515,35 +515,48 @@ using LinearAlgebra
         GravityFactor  = SimParticles.GravityFactor
         MotionLimiter  = SimParticles.MotionLimiter
 
-        @inbounds for i in eachindex(Position)
-            if GhostKernel[i] >= 0.1
-                if abs(det(Aᵧ[i])) >= 1e-3
-                    InvA = inv(Aᵧ[i])
-                    InfNormA    = opnorm(Aᵧ[i], Inf)
-                    InfNormInvA = opnorm(InvA, Inf)
-                    condinf     = SimConstants.dx^2 * InfNormA * InfNormInvA
+        ρ₀ = SimConstants.ρ₀
 
-                    if condinf <= 50
+        #https://github.com/DualSPHysics/DualSPHysics/blob/f4fa76ad5083873fa1c6dd3b26cdce89c55a9aeb/src/source/JSphCpu_mdbc.cpp#L347
+        @inbounds for i in eachindex(Position)
+            # if GhostKernel[i] >= 0.1
+                if abs(det(Aᵧ[i])) >= 1e-3
+                    # InvA = inv(Aᵧ[i])
+                    # InfNormA    = opnorm(Aᵧ[i], Inf)
+                    # InfNormInvA = opnorm(InvA, Inf)
+                    # condinf     = SimConstants.dx^2 * InfNormA * InfNormInvA
+
+                    # if condinf <= 50
                         GhostPointDensity = Aᵧ[i] \ bᵧ[i]
-                        v1 = GhostPointDensity[1] + dot(Positionₙ⁺[i] - GhostPoints[i], GhostPointDensity[2:end])
+                        v1 = first(GhostPointDensity) + dot(Positionₙ⁺[i] - GhostPoints[i], GhostPointDensity[2:end])
 
                         # Almost equal, properly some matrix math
                         # v2 = InvA[1,1] * bᵧ[i][1] + InvA[1,2] * bᵧ[i][2] + InvA[1,3] * bᵧ[i][3]
 
                         # println("v1:", v1, "| v2:", v2)
 
-                        ρₙ⁺[i] = v1
-                    else
-                        ρₙ⁺[i] = first(bᵧ[i]) / Aᵧ[i][1,1]
-                    end
+                        if !isnan(v1)
+                            ρₙ⁺[i] = v1
+                        else
+                            ρₙ⁺[i] = ρ₀
+                        end
+                    # else
+                    #     ρₙ⁺[i] = first(bᵧ[i]) / Aᵧ[i][1,1]
+                    # end
 
                 else
-                    ρₙ⁺[i] = first(bᵧ[i]) / Aᵧ[i][1,1]
+                    v = first(bᵧ[i]) / first(Aᵧ[i])
+                    if !isnan(v)
+                        ρₙ⁺[i] = v
+                    else
+                        ρₙ⁺[i] = ρ₀
+                    end
                 end
-            else
-                # This does not work
-                # ρₙ⁺[i] =  max(SimConstants.ρ₀, first(bᵧ[i]) / first(Aᵧ[i]))
-            end
+            # end
+            # else
+            #     # This does not work
+            #     ρₙ⁺[i] =  max(SimConstants.ρ₀, first(bᵧ[i]) / first(Aᵧ[i]))
+            # end
         end
 
         @inbounds for i in eachindex(Position)
