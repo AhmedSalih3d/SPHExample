58db772cb9b63618a3f8882c6006bae119154496
AhmedSalih3d <36305327+AhmedSalih3d@users.noreply.github.com>
2025-04-08 00:45:57 +0200

    Save progress Alhamdu lillah

--- example/StillWedgeMDBC.jl
+++ example/StillWedgeMDBC.jl
@@ -5,7 +5,7 @@ let
     FloatType  = Float64
 
     
-    SimConstantsWedge = SimulationConstants{FloatType}(dx=0.02,c₀=42.48576250492629, δᵩ = 0.1, CFL=0.2)
+    SimConstantsWedge = SimulationConstants{FloatType}(dx=0.02,c₀=42.48576250492629, δᵩ = 0.1, CFL=0.5)
     # SimConstantsWedge = SimulationConstants{FloatType}(dx=0.01,c₀=43.4, δᵩ = 0.1, CFL=0.2)
 
     # Assuming SimConstantsWedge is defined somewhere else with the field `dx`
@@ -31,7 +31,7 @@ let
     SimMetaDataWedge  = SimulationMetaData{Dimensions,FloatType}(
         SimulationName="StillWedge", 
         SaveLocation="E:/SecondApproach/TESTING_CPU_StillWedge",
-        SimulationTime=1,
+        SimulationTime=4,
         OutputEach=0.01,
         VisualizeInParaview=true,
         ExportSingleVTKHDF=true,
