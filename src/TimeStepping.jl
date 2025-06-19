861a783c424aeb3bdc49f495876c5cc22f0b3c25
AhmedSalih3d <36305327+AhmedSalih3d@users.noreply.github.com>
2025-06-20 00:04:20 +0200

    Small update to layout of time stepping function, cleaner and clearer code

--- src/TimeStepping.jl
+++ src/TimeStepping.jl
@@ -1,38 +1,48 @@
 module TimeStepping
 
-    export Δt
+export Δt
 
-    using LinearAlgebra
-    using Parameters
+using LinearAlgebra
+using Parameters
 
-    # A few time stepping controls implemented to allow for an adaptive time step
-    function Δt(Position, Velocity, Acceleration, SimulationConstants, SPHKernel)
-        @unpack c₀, CFL = SimulationConstants
-        @unpack h, η²   = SPHKernel
+"""
+    Δt(Position, Velocity, Acceleration, SimulationConstants, SPHKernel)
 
-        function max_visc(Velocity,Position,h,η²)
-            maxval = -Inf
-            for i in eachindex(Velocity,Position)
-                 tmp = abs(h * dot(Velocity[i],Position[i]) / (dot(Position[i],Position[i]) + η²))
-                 tmp > maxval && (maxval = tmp)
-            end
-            return maxval
-         end
-        
-        visc = max_visc(Velocity,Position,h,η²)
+Calculates the adaptive time step for the simulation based on Courant-Friedrichs-Lewy (CFL),
+viscous, and force-based criteria.
 
-        dt1 = Inf
-        for Acceleration_ in Acceleration
-            dt1_candidate = sqrt(h / norm(Acceleration_))
-            dt1 = min(dt1, dt1_candidate)
-        end
+# Arguments
+- `Position`: Vector of position vectors for each particle.
+- `Velocity`: Vector of velocity vectors for each particle.
+- `Acceleration`: Vector of acceleration vectors for each particle.
+- `SimulationConstants`: Struct containing simulation parameters like `c₀` (speed of sound) and `CFL` number.
+- `SPHKernel`: Struct containing kernel parameters like `h` (smoothing length) and `η²`.
 
+# Returns
+- The calculated time step `dt`.
+"""
+function Δt(Position, Velocity, Acceleration, SimulationConstants, SPHKernel)
+    @unpack c₀, CFL = SimulationConstants
+    @unpack h, η²   = SPHKernel
 
-        dt2   = h / (c₀+visc)
-
-        dt    = CFL*min(dt1,dt2)
-
-        return dt
+    # Viscous timestep constraint
+    # More idiomatic and potentially faster version of max_visc
+    visc = maximum(zip(Velocity, Position)) do (v, r)
+        abs(h * dot(v, r) / (dot(r, r) + η²))
     end
+
+    # Force-based timestep constraint
+    # Use a generator expression with `minimum` for conciseness and efficiency
+    # The norm of the acceleration is calculated for each particle.
+    dt1 = minimum(sqrt(h / norm(acc)) for acc in Acceleration; init=Inf)
+
+    # Courant-like speed of sound condition
+    dt2 = h / (c₀ + visc)
+
+    # Final timestep is the minimum of the two, scaled by the CFL number
+    dt = CFL * min(dt1, dt2)
+
+    return dt
+end
 
 end
