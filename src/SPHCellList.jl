ef237971c0242d48b576a74e4e2fc5ae5ad7b6e0
AhmedSalih3d <36305327+AhmedSalih3d@users.noreply.github.com>
2025-12-27 15:23:47 +0100

    Avoid Cells slice in loop

--- src/SPHCellList.jl
+++ src/SPHCellList.jl
@@ -137,7 +137,7 @@ using LinearAlgebra
         empty!(CellDict)
         CellDict[Cells[1]] = IndexCounter
 
-        @inbounds @simd ivdep for i in eachindex(Cells)[2:end]
+        @inbounds @simd ivdep for i in 2:lastindex(Cells)
             if Cells[i] != Cells[i-1] # Equivalent to diff(Cells) != 0
                 IndexCounter                 += 1
                 ParticleRanges[IndexCounter]  = i
