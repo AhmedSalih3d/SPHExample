cca0bc553edffb3105efe72794391a55105a7ef0
AhmedSalih3d <36305327+AhmedSalih3d@users.noreply.github.com>
2026-01-04 13:04:14 +0100

    Cache cell list index per particle

--- src/PreProcess.jl
+++ src/PreProcess.jl
@@ -108,9 +108,10 @@ function AllocateDataStructures(SimGeometry::Vector{<:Geometry{Dimensions, Float
 
     Pressureᵢ      = zeros(PositionUnderlyingType, NumberOfPoints)
     
-    Cells          = fill(zero(CartesianIndex{Dimensions}), NumberOfPoints)
-    
-    SimParticles = StructArray((Cells = Cells, Kernel = Kernel, KernelGradient = KernelGradient, Position=Position, Acceleration=Acceleration, Velocity=Velocity, Density=Density, Pressure=Pressureᵢ, GravityFactor=GravityFactor, MotionLimiter=MotionLimiter, BoundaryBool = BoundaryBool, ID = Idp , Type = Types, GroupMarker = GroupMarker, GhostPoints = GhostPoints, GhostNormals=GhostNormals))
+    Cells         = fill(zero(CartesianIndex{Dimensions}), NumberOfPoints)
+    CellListIndex = zeros(Int, NumberOfPoints)
+
+    SimParticles = StructArray((Cells = Cells, CellListIndex = CellListIndex, Kernel = Kernel, KernelGradient = KernelGradient, Position=Position, Acceleration=Acceleration, Velocity=Velocity, Density=Density, Pressure=Pressureᵢ, GravityFactor=GravityFactor, MotionLimiter=MotionLimiter, BoundaryBool = BoundaryBool, ID = Idp , Type = Types, GroupMarker = GroupMarker, GhostPoints = GhostPoints, GhostNormals=GhostNormals))
 
     sort!(SimParticles, by = p -> p.ID)
 
