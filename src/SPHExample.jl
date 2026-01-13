058744fe6636ec942544329af5035c98fea3fde2
AhmedSalih3d <36305327+AhmedSalih3d@users.noreply.github.com>
2026-01-13 20:23:43 +0100

    Fix precompile workload

--- src/SPHExample.jl
+++ src/SPHExample.jl
@@ -38,7 +38,7 @@ module SPHExample
     using .ProduceHDFVTK
     export SaveVTKHDF, GenerateGeometryStructure, GenerateStepStructure, AppendVTKHDFData, SaveCellGridVTKHDF, AppendVTKHDFGridData, SetupVTKOutput
 
-    using .TimeStepping: Δt
+    using .TimeStepping: Δt, FinalizeTimeStep, UpdateTimeStepBuffers!, HalfTimeStep, FullTimeStep
     export Δt
 
     using .SimulationEquations
@@ -66,22 +66,28 @@ module SPHExample
     export AutoOpenLogFile, AutoOpenParaview
 
     using PrecompileTools
+    using LinearAlgebra
     using StaticArrays
     using StructArrays
 
     @setup_workload begin
         KernelInstance = SPHKernelInstance{2, Float64}(WendlandC2(); dx=0.02)
-        Constants = SimulationConstants()
-        Position = [SVector(0.0, 0.0), SVector(0.02, 0.0)]
-        Velocity = [SVector(0.0, 0.0), SVector(0.1, 0.0)]
-        Acceleration = [SVector(0.0, 0.0), SVector(0.0, -9.81)]
-        Density = fill(Constants.ρ₀, 2)
-        Pressure = zeros(Float64, 2)
-        GravityFactor = [-1.0, 1.0]
-        MotionLimiter = [1.0, 0.0]
-        Types = [Fluid, Moving]
-        GroupMarker = [UInt(1), UInt(1)]
-        Cells = fill(CartesianIndex{2}(0, 0), 2)
+        CubicKernelInstance = SPHKernelInstance{2, Float64}(CubicSpline{Float64}(); dx=0.02)
+        Constants = SimulationConstants(ρ₀ = 1000.0, dx = 0.02, γ = 7.0)
+        Position = [SVector(0.0, 0.0), SVector(0.02, 0.0), SVector(0.04, 0.0)]
+        Velocity = [SVector(0.0, 0.0), SVector(0.1, 0.0), SVector(-0.05, 0.0)]
+        Acceleration = [SVector(0.0, 0.0), SVector(0.0, -9.81), SVector(0.0, -9.81)]
+        Density = fill(Constants.ρ₀, 3)
+        Pressure = zeros(Float64, 3)
+        GravityFactor = [-1.0, 1.0, -1.0]
+        MotionLimiter = [1.0, 0.0, 1.0]
+        Types = [Fluid, Moving, Fluid]
+        GroupMarker = [UInt(1), UInt(1), UInt(2)]
+        Cells = fill(CartesianIndex{2}(0, 0), 3)
+        Kernel = zeros(Float64, 3)
+        KernelGradient = zeros(eltype(Position), 3)
+        GhostPoints = zeros(eltype(Position), 3)
+        GhostNormals = zeros(eltype(Position), 3)
         SimParticles = StructArray((;
             Cells = Cells,
             Position = Position,
@@ -93,32 +99,49 @@ module SPHExample
             MotionLimiter = MotionLimiter,
             Type = Types,
             GroupMarker = GroupMarker,
+            Kernel = Kernel,
+            KernelGradient = KernelGradient,
+            GhostPoints = GhostPoints,
+            GhostNormals = GhostNormals,
         ))
         Xij = Position[1] - Position[2]
         Vij = Velocity[1] - Velocity[2]
         DistanceSquared = dot(Xij, Xij)
         Q = sqrt(DistanceSquared) * KernelInstance.h⁻¹
         GradientWij = ∇Wᵢⱼ(KernelInstance, Q, Xij)
-        SimMetaData = SimulationMetaData{2, Float64}(SimulationName = "Precompile", SaveLocation = ".")
-        PositionNext = similar(Position)
-        VelocityNext = similar(Velocity)
-        DensityNext = similar(Density)
-        DensityRate = zeros(Float64, 2)
+        CubicGradientWij = ∇Wᵢⱼ(CubicKernelInstance, Q, Xij)
+        SimMetaData = SimulationMetaData{2, Float64, NoShifting, StoreKernelOutput, SimpleMDBC, NoLog}(SimulationName = "Precompile", SaveLocation = ".")
+        DensityRate, VelocityNext, PositionNext, DensityNext, ShiftGradient, ShiftDivergence = AllocateSupportDataStructures(SimMetaData, Position)
         HalfStep = 0.5
+        FullStep = 0.01
+        MaxVisc = zeros(Float64, 3)
+        MinDtForce = fill(Inf, 3)
 
         @compile_workload begin
             Wᵢⱼ(KernelInstance, Q)
             ∇Wᵢⱼ(KernelInstance, Q, Xij)
             tensile_correction(KernelInstance, 0.0, 1.0, 0.0, 1.0, Q, 0.02)
+            Wᵢⱼ(CubicKernelInstance, Q)
+            ∇Wᵢⱼ(CubicKernelInstance, Q, Xij)
+            tensile_correction(CubicKernelInstance, 0.0, 1.0, 0.0, 1.0, Q, 0.02)
             EquationOfStateGamma7(Constants.ρ₀, Constants.c₀, Constants.ρ₀)
             ConstructGravitySVector(Xij, Constants.g)
             Pressure!(Pressure, Density, Constants)
             LimitDensityAtBoundary!(Density, Constants.ρ₀, MotionLimiter)
+            compute_viscosity(ZeroViscosity(), KernelInstance, Constants, SimParticles, Xij, Vij, GradientWij, DistanceSquared, 1, 2)
             compute_viscosity(ArtificialViscosity(), KernelInstance, Constants, SimParticles, Xij, Vij, GradientWij, DistanceSquared, 1, 2)
             compute_viscosity(Laminar(), KernelInstance, Constants, SimParticles, Xij, Vij, GradientWij, DistanceSquared, 1, 2)
+            compute_viscosity(LaminarSPS(), KernelInstance, Constants, SimParticles, Xij, Vij, GradientWij, DistanceSquared, 1, 2)
+            compute_density_diffusion(ZeroDensityDiffusion(), KernelInstance, Constants, SimParticles, Xij, GradientWij, DistanceSquared, 1, 2, MotionLimiter)
             compute_density_diffusion(LinearDensityDiffusion(), KernelInstance, Constants, SimParticles, Xij, GradientWij, DistanceSquared, 1, 2, MotionLimiter)
             compute_density_diffusion(ComplexDensityDiffusion(), KernelInstance, Constants, SimParticles, Xij, GradientWij, DistanceSquared, 1, 2, MotionLimiter)
+            compute_density_diffusion(LinearDensityDiffusion(), CubicKernelInstance, Constants, SimParticles, Xij, CubicGradientWij, DistanceSquared, 1, 2, MotionLimiter)
             HalfTimeStep(SimMetaData, Constants, SimParticles, PositionNext, VelocityNext, DensityNext, DensityRate, HalfStep)
+            FullTimeStep(SimMetaData, KernelInstance, Constants, SimParticles, ShiftGradient, ShiftDivergence, FullStep)
+            UpdateTimeStepBuffers!(MaxVisc, MinDtForce, 1, Position[1], Velocity[1], Acceleration[1], KernelInstance)
+            UpdateTimeStepBuffers!(MaxVisc, MinDtForce, 2, Position[2], Velocity[2], Acceleration[2], KernelInstance)
+            FinalizeTimeStep(MaxVisc, MinDtForce, Constants, KernelInstance)
+            Δt(Position, Velocity, Acceleration, Constants, KernelInstance)
         end
     end
 
