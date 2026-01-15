8e722e479b94a17c934698780ee3a7b9b96443b3
AhmedSalih3d <36305327+AhmedSalih3d@users.noreply.github.com>
2026-01-15 23:40:11 +0100

    Fix CUDA timestep kernel

--- src/SPHCUDA.jl
+++ src/SPHCUDA.jl
@@ -27,7 +27,7 @@ using StructArrays: StructArray
 
 CUDAAvailable() = CUDA.functional()
 
-function ΔtCUDAKernel!(max_visc, min_dt_force, Position, Velocity, Acceleration, h, η²)
+function ΔtCUDAKernel!(visc_buffer, dt_buffer, Position, Velocity, Acceleration, h, η²)
     i = (blockIdx().x - 1) * blockDim().x + threadIdx().x
     if i <= length(Position)
         r = Position[i]
@@ -36,27 +36,23 @@ function ΔtCUDAKernel!(max_visc, min_dt_force, Position, Velocity, Acceleration
         r_sq = sqrt(dot(r, r))^2
         curr_visc = abs(h * dot(v, r) / (r_sq + η²))
         a_mag = norm(a)
-        curr_dt_force = a_mag > 0 ? sqrt(h / a_mag) : typemax(eltype(a_mag))
-        CUDA.atomic_max!(max_visc, 1, curr_visc)
-        CUDA.atomic_min!(min_dt_force, 1, curr_dt_force)
+        curr_dt_force = a_mag > 0 ? sqrt(h / a_mag) : oftype(a_mag, Inf)
+        visc_buffer[i] = curr_visc
+        dt_buffer[i] = curr_dt_force
     end
     return nothing
 end
 
-@inline function ΔtCUDA(Position, Velocity, Acceleration, SimulationConstants, SPHKernel)
+@inline function ΔtCUDA!(visc_buffer, dt_buffer, Position, Velocity, Acceleration, SimulationConstants, SPHKernel)
     @unpack c₀, CFL = SimulationConstants
     @unpack h, η² = SPHKernel
 
-    scalar_type = eltype(eltype(Position))
-    max_visc = CUDA.zeros(scalar_type, 1)
-    min_dt_force = CUDA.fill(typemax(scalar_type), 1)
-
     threads = 256
     blocks = cld(length(Position), threads)
-    @cuda threads=threads blocks=blocks ΔtCUDAKernel!(max_visc, min_dt_force, Position, Velocity, Acceleration, h, η²)
+    @cuda threads=threads blocks=blocks ΔtCUDAKernel!(visc_buffer, dt_buffer, Position, Velocity, Acceleration, h, η²)
 
-    global_visc = Array(max_visc)[1]
-    global_dt_force = Array(min_dt_force)[1]
+    global_visc = CUDA.reduce(max, visc_buffer)
+    global_dt_force = CUDA.reduce(min, dt_buffer)
     dt2 = h / (c₀ + global_visc)
     return CFL * min(global_dt_force, dt2)
 end
@@ -327,7 +323,11 @@ end
     velocity_step_gpu = CuArray(Velocity)
     acceleration_step_gpu = CuArray(Acceleration)
 
-    dt = ΔtCUDA(position_step_gpu, velocity_step_gpu, acceleration_step_gpu, SimConstants, SimKernel)
+    scalar_type = eltype(eltype(Position))
+    visc_buffer_gpu = CUDA.zeros(scalar_type, length(Position))
+    dt_buffer_gpu = CUDA.zeros(scalar_type, length(Position))
+
+    dt = ΔtCUDA!(visc_buffer_gpu, dt_buffer_gpu, position_step_gpu, velocity_step_gpu, acceleration_step_gpu, SimConstants, SimKernel)
 
     dt₂ = dt * 0.5
 
@@ -393,7 +393,7 @@ end
         CUDA.copyto!(position_step_gpu, Positionₙ⁺)
         CUDA.copyto!(velocity_step_gpu, Velocityₙ⁺)
         CUDA.copyto!(acceleration_step_gpu, Acceleration)
-        @timeit SimMetaData.HourGlass "13 Update TimeStep" dt = ΔtCUDA(position_step_gpu, velocity_step_gpu, acceleration_step_gpu, SimConstants, SimKernel)
+        @timeit SimMetaData.HourGlass "13 Update TimeStep" dt = ΔtCUDA!(visc_buffer_gpu, dt_buffer_gpu, position_step_gpu, velocity_step_gpu, acceleration_step_gpu, SimConstants, SimKernel)
         dt₂ = dt * 0.5
     end
 
