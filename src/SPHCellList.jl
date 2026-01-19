6c6976aaa41ddde5a3afca06418fa3931eff345f
AhmedSalih3d <36305327+AhmedSalih3d@users.noreply.github.com>
2026-01-20 00:20:33 +0100

    Remove const declaration around FullStencil variable

--- src/SPHCellList.jl
+++ src/SPHCellList.jl
@@ -304,7 +304,7 @@ using LinearAlgebra
                                Density, GhostPoints, GhostNormals, ParticleType,
                                bᵧ, Aᵧ) where {Dimensions, FloatType, SMode, KMode, BMode, LMode}
         
-        const FullStencil = ConstructStencil(Val(Dimensions))
+        FullStencil = ConstructStencil(Val(Dimensions))
 
         @inbounds @threads for iter in eachindex(GhostPoints)
             GhostPoint = GhostPoints[iter]
