aca505f8f7ab7bba581a2abafeced4a0d9973cd8
AhmedSalih3d <36305327+AhmedSalih3d@users.noreply.github.com>
2026-01-28 19:00:52 +0100

    Clarify moving square shifting default

--- example/MovingSquare2d.jl
+++ example/MovingSquare2d.jl
@@ -13,7 +13,7 @@ let
         Cb = 112000,
         α  = 1e-6,
         CFL=0.2,
-        A  = 0.01
+        A  = 0.01 # Default shifting value from earlier commits
     )
 
     SimMetaDataMovingSquare  = SimulationMetaData{Dimensions,FloatType,PlanarShifting,NoKernelOutput,NoMDBC,StoreLog}(
