cffc4c118ae361db56a26585c8ca20d115916bf0
AhmedSalih3d <36305327+AhmedSalih3d@users.noreply.github.com>
2026-07-06 01:05:54 +0200

    Dispatch time stepping loops

--- example/DucklingMDBC.jl
+++ example/DucklingMDBC.jl
@@ -60,7 +60,6 @@ let
         SimKernel           = SimKernel,
         SimViscosity        = SimViscosity,
         SimDensityDiffusion = SimDensityDiffusion,
-        SimTimeStepping     = SingleNeighborTimeStepping(),
         ParticleNormalsPath = "./input/case_duckling_mdbc/CaseDuckling_Dp$(SimConstantsWedge.dx)_GhostNodes.csv"
     )
 
