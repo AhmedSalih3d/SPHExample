497df255af544fee8b186d6b97dea6349a4ccd15
AhmedSalih3d <36305327+AhmedSalih3d@users.noreply.github.com>
2026-08-13 15:38:01 +0200

    Re-apply SimpleMDBC

--- example/StillWedgeMDBC.jl
+++ example/StillWedgeMDBC.jl
@@ -7,7 +7,7 @@ let
     SimConstantsWedge = SimulationConstants{FloatType}(dx=0.02,c₀=42.48576250492629, δᵩ = 0.1, CFL=0.5)
     # SimConstantsWedge = SimulationConstants{FloatType}(dx=0.01,c₀=43.4, δᵩ = 0.1, CFL=0.2)
 # 
-    SimMetaDataWedge  = SimulationMetaData{Dimensions,FloatType,NoShifting,NoKernelOutput,NoMDBC,StoreLog}(
+    SimMetaDataWedge  = SimulationMetaData{Dimensions,FloatType,NoShifting,NoKernelOutput,SimpleMDBC,StoreLog}(
         SimulationName="StillWedge", 
         SaveLocation="W:/Simulations/StillWedge2D_MDBC",
         SimulationTime=4.0,
